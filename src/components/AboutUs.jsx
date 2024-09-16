b469e8d91f6e4b966f6a075aabdac93a111804d0
nurs_996 <98372@gamil.com>
2024-09-16 13:06:53 +0600

    bug fix

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -131,8 +131,8 @@ const StyledLoremText = styled("p")(({ theme }) => ({
 		maxWidth: "100%",
   },
   [theme.breakpoints.down("lg")]: {
-    fontSize: "20px",
-    lineHeight: "24px",
+    fontSize: "22px",
+    // lineHeight: "24px",
     maxWidth: "100%",
   }
 }));
